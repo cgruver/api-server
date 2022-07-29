5698fa4f97ac785955cae0ac132b1b3674981607
Charro Gruver <charro_gruver@yahoo.com>
2022-07-29 15:48:52 -0400

    wip

--- src/main/java/org/labmonkeys/apiserver/dto/MessageDto.java
+++ src/main/java/org/labmonkeys/apiserver/dto/MessageDto.java
@@ -2,14 +2,4 @@ package org.labmonkeys.apiserver.dto;
 
 import java.util.UUID;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class MessageDto {
-    UUID messageId;
-    String message;
-}
+public record MessageDto(UUID messageId, String message){}
